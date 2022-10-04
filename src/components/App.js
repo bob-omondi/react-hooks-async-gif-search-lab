1e2cdfdfb464dd73d30622f836d2da1b90255007
+ <+>
2022-10-04 23:21:33 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import GifListContainer from "./GifListContainer";
 import NavBar from "./NavBar";
 
 // the App component should render out the GifListContainer component
@@ -8,8 +8,15 @@ function App() {
   return (
     <div>
       <NavBar color="black" title="Giphy Search" />
+      <GifListContainer />
     </div>
   );
 }
 
 export default App;
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
---
+++ src/components/GifList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GifList({ gif }) {
+  return (
+    <li>
+      <img src={gif.images.original.url} alt="gif" />
+    </li>
+  );
+}
+
+export default GifList;
